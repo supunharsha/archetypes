5d3fd1d1d1e99bdbaa03251670f4f0a0a97cf4a9
Supun <supunp@wso2.com>
2017-11-16 14:52:53 +0530

    Added is-user-store-manager archetype

---
+++ is-user-store-manager-archetype/src/main/resources/archetype-resources/src/main/java/__user_store_name__UserStoreManager.java
@@ -0,0 +1,7 @@
+package ${package};
+
+public class ${user_store_name}UserStoreManager extends AbstractUserStoreManager {
+
+    private static final Log log = LogFactory.getLog(${user_store_name}UserStoreManager.class);
+
+}
