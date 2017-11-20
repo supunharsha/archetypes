440eba881a603d2e8299b1beeaf1da7e6f24eeaf
Supun <supunp@wso2.com>
2017-11-20 09:59:46 +0530

    Added common methods

--- is-user-store-manager-archetype/src/main/resources/archetype-resources/src/main/java/__user_store_name__UserStoreManager.java
+++
@@ -1,7 +0,0 @@
-package ${package};
-
-public class ${user_store_name}UserStoreManager extends AbstractUserStoreManager {
-
-    private static final Log log = LogFactory.getLog(${user_store_name}UserStoreManager.class);
-
-}
